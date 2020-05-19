dc6508eff6647c21b4f2c76cbd8f7d977fda802f
181268 <lujieni1017>
2020-05-19 15:09:41 +0800

    first commit

---
+++ src/main/java/com/lujieni/zuul/config/AuthFilter.java
@@ -0,0 +1,62 @@
+package com.lujieni.zuul.config;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Auther lujieni
+ * @Date 2020/5/19
+ */
+@Component
+public class AuthFilter extends ZuulFilter {
+    private Logger logger = LoggerFactory.getLogger(AuthFilter.class);
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        /* 获取请求上下文 */
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        /* 获取到request */
+        HttpServletRequest request = requestContext.getRequest();
+        String uri = request.getRequestURI();
+        logger.info("===uri==={}", uri);
+        if("/api/wahaha/zuul".equals(uri)){
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+            requestContext.setResponseBody("no auth");//加了chrome中才会显示401
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("run");
+        return null;
+    }
+
+    /**
+     * 设置 400 无权限状态
+     */
+    private void setUnauthorizedResponse(RequestContext requestContext, String msg) {
+        requestContext.setSendZuulResponse(false);
+        requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+}
